7b170305b0307c9c09a052ccad83e14a4abb48a4
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-09-19 15:02:15 -0600

    css added

--- app/studentInfo.js
+++ app/studentInfo.js
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function StudentInfo() {
     return (
-        <div className="text-center , bg-#A52A2A">
+        <div className="text-center p-20  m-20" style={{ backgroundColor: '#787878' , color: '#FFFFFF' }}>
             <p> Name: Mausami Desai </p>
             <p> Course Section: CPRG306 A </p>
             <Link href ="https://github.com/mosmee21/cprg306-assignments.git">https://github.com</Link>
+            <a className="custom-link">https://github.com</a>
+            
         </div>
     )
 }
