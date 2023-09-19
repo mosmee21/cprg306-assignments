b445105168e897f099a4749c9d50d5c22cdcbc0b
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-09-19 12:30:12 -0600

    add info

---
+++ app/studentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Name: Moshf</p>
+            <p>Course section: Cprg 306 A</p>
+            <Link href="https://github.com/mosmee21/cprg306-assignments.git">https://github.com</Link>
+        </main>
+    )
+}
