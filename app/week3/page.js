fdf2f88e9e11d2340f6d0481607a4a4bc9085cb1
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-09-25 13:33:01 -0600

    Update page.js

--- app/week3/page.js
+++ app/week3/page.js
@@ -4,7 +4,7 @@ import ItemList from './item-list';  // Import the ItemList component
 const Page = () => {
   return (
     <main className="min-h-screen bg-gray-100 py-8">
-      <h1 className="text-center text-3xl font-bold mb-6 text-blue-600">Shopping List</h1>
+      <h1 className="text-center text-3xl font-bold mb-6 text-blue-900">Shopping List</h1>
       <div className="container mx-auto max-w-2xl">
         <ItemList />
       </div>
