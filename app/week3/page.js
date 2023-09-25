84db25b0cbc911056c4f33e96bae3dcd6a174c2a
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-09-25 13:12:34 -0600

    item list added

---
+++ app/week3/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ItemList from './item-list';  // Import the ItemList component
+
+const Page = () => {
+  return (
+    <main className="min-h-screen bg-gray-100 py-8">
+      <h1 className="text-center text-3xl font-bold mb-6 text-blue-600">Shopping List</h1>
+      <div className="container mx-auto max-w-2xl">
+        <ItemList />
+      </div>
+    </main>
+  );
+};
+
+export default Page;
