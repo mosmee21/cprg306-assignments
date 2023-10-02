20075e15bdfb1915bc943dd52e5e72e458442a32
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-02 17:16:24 -0600

    Update new-item.js

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -1,82 +1,83 @@
-/* @jsxImportSource react */
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
-function NewItem() {
+export default function NewItem() {
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(1);
   const [category, setCategory] = useState("Produce");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     const item = { name, quantity, category };
     console.log(item);
-    alert(`Name: ${name}\nQuantity: ${quantity}\nCategory: ${category}`);
     setName("");
     setQuantity(1);
     setCategory("Produce");
   };
 
   return (
-    <div className="p-6 bg-white">
-      <form onSubmit={handleSubmit} className="p-4">
-        <div className="mb-4">
-          <label className="text-lg mb-2" htmlFor="name">
-            Name:
-          </label>
-          <input 
-            type="text" 
-            value={name} 
-            onChange={(e) => setName(e.target.value)} 
-            required 
-            className="p-2 border"
-            id="name"
-          />
+    <main>
+      <div className="min-h-screen bg-blue-gray-100 flex items-center justify-center">
+        <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+          <h1 className="text-2xl text-gray-800 font-bold mb-8">
+            Create New Item
+          </h1>
+          <form onSubmit={handleSubmit}>
+            <label className="block mb-4">
+              <span className="text-gray-800">Item Name:</span>
+              <input
+                type="text"
+                required
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                className="mt-1 p-1 block w-full rounded-md text-black bg-gray-100 focus:bg-white"
+              />
+            </label>
+
+            <label className="block mb-4">
+              <span className="text-gray-800">Quantity:</span>
+              <input
+                type="number"
+                min="1"
+                max="99"
+                required
+                onChange={(e) => setQuantity(Number(e.target.value))}
+                value={quantity}
+                className="mt-1 p-1 block w-full rounded-md text-black bg-gray-100 focus:bg-white"
+              />
+            </label>
+
+            <label className="block mb-4">
+              <span className="text-gray-800">Category:</span>
+              <select
+                required
+                onChange={(e) => setCategory(e.target.value)}
+                value={category}
+                className="mt-1 p-1 block w-full rounded-md text-black bg-gray-100 focus:bg-white"
+              >
+                <option value="Produce">Produce</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Bakery">Bakery</option>
+                <option value="Meat">Meat</option>
+                <option value="Frozen Foods">Frozen Foods</option>
+                <option value="Canned Goods">Canned Goods</option>
+                <option value="Dry Goods">Dry Goods</option>
+                <option value="Beverages">Beverages</option>
+                <option value="Snacks">Snacks</option>
+                <option value="Household">Household</option>
+                <option value="Other">Other</option>
+              </select>
+            </label>
+
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-sky-600 hover:bg-sky-500 rounded-md text-white"
+            >
+              Create Item
+            </button>
+          </form>
         </div>
-        <div className="mb-4">
-          <label className="text-lg mb-2" htmlFor="quantity">
-            Quantity:
-          </label>
-          <input 
-            type="number" 
-            min="1" 
-            max="99" 
-            value={quantity} 
-            onChange={(e) => setQuantity(Number(e.target.value))} 
-            required 
-            className="p-2 border"
-            id="quantity"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-lg mb-2" htmlFor="category">
-            Category:
-          </label>
-          <select 
-            value={category} 
-            onChange={(e) => setCategory(e.target.value)} 
-            required 
-            className="p-2 border"
-            id="category"
-          >
-            <option value="Produce">Produce</option>
-            <option value="Dairy">Dairy</option>
-            <option value="Bakery">Bakery</option>
-            <option value="Meat">Meat</option>
-            <option value="Frozen Foods">Frozen Foods</option>
-            <option value="Canned Goods">Canned Goods</option>
-            <option value="Dry Goods">Dry Goods</option>
-            <option value="Beverages">Beverages</option>
-            <option value="Snacks">Snacks</option>
-            <option value="Household">Household</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
-        <button type="submit" className="p-2 bg-blue-500 text-white">
-          Submit
-        </button>
-      </form>
-    </div>
+      </div>
+    </main>
   );
 }
-
-export default NewItem;
