98e6a1dfb8d2d2748d78ce86efb7836ac41e2b5f
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-02 17:20:26 -0600

    Update new-item.js

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -19,12 +19,10 @@ export default function NewItem() {
     <main>
       <div className="min-h-screen bg-blue-gray-100 flex items-center justify-center">
         <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
-          <h1 className="text-2xl text-gray-800 font-bold mb-8">
-            Create New Item
-          </h1>
+
           <form onSubmit={handleSubmit}>
             <label className="block mb-4">
-              <span className="text-gray-800">Item Name:</span>
+              <span className="text-gray-800">Name:</span>
               <input
                 type="text"
                 required
@@ -73,7 +71,7 @@ export default function NewItem() {
               type="submit"
               className="w-full py-2 px-4 bg-sky-600 hover:bg-sky-500 rounded-md text-white"
             >
-              Create Item
+              Submit
             </button>
           </form>
         </div>
