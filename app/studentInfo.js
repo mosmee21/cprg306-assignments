4ce71b19b07ab0841366e02edd365211acebe7be
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-08 10:11:29 -0600

    css added

--- app/studentInfo.js
+++ app/studentInfo.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function StudentInfo() {
     return (
-        <div className="text-center text-red-900 font-bold" >
+        <div className="text-center text-blue-500 font-bold" >
             <p> Name: Mausami Desai </p>
             <p> Course Section: CPRG306 A </p>
             <Link href ="https://github.com/mosmee21/cprg306-assignments.git">https://github.com</Link>
--- app/week2/page.js
+++ app/week2/page.js
@@ -1,8 +1,8 @@
 import StudentInfo from "../studentInfo";
 export default function week2() {
     return(
-        <div className="text-center">
-            <h1 className="font-bold , text-xl mt-10 mb-10">My Shopping List</h1>
+        <div className="text-center ,p-10 m-10">
+            <h1 className="font-bold , text-white, text-mt-10 mb-10">My Shopping List</h1>
             <StudentInfo/>
         </div>
     )
