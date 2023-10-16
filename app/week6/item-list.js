10a00ae436b06c1f6a60f735773791849f878fa7
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-16 17:36:39 -0600

    edit pages

--- app/week6/item-list.js
+++ app/week6/item-list.js
@@ -2,12 +2,12 @@
 
 import { useState } from 'react';
 import Item from './item';
-import itemsData from './items.json';
+
 
 export default function ItemList() {
   const [sortBy, setSortBy] = useState("name");
   
-  let items = [...itemsData];
+  let items = [...itemsProp];
 
   let groupedItems = {};
 
@@ -29,19 +29,19 @@ export default function ItemList() {
       <div className="p-4">
         <button
           onClick={() => setSortBy('name')}
-          className={`${sortBy === 'name'} m-5 bg-orange-900 text-amber-300 px-2 py-2 mr-4 `}
+          className={`${sortBy === 'name'? 'active-class' : ''} m-5 bg-orange-900 text-amber-300 px-2 py-2 mr-4 `}
         >
           Sort by Name
         </button>
         <button
         onClick={() => setSortBy('category')}
-        className={`${sortBy === 'category'} bg-orange-900 text-amber-300 px-2 py-2 ml-2 `}
+        className={`${sortBy === 'category' ? 'active-class' : ''} bg-orange-900 text-amber-300 px-2 py-2 ml-2 `}
       >
         Sort by Category
       </button>
       <button
         onClick={() => setSortBy('grouped')}
-        className={`${sortBy === 'grouped'} bg-orange-900 text-amber-300 px-4 py-2 ml-2 `}
+        className={`${sortBy === 'grouped' ? 'active-class' : ''} bg-orange-900 text-amber-300 px-4 py-2 ml-2 `}
         >
         Group by Category
       </button>
