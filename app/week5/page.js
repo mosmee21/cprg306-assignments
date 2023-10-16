ba158baf0447e11b54bb050971dda8d5b10dabcf
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-16 13:21:05 -0600

    Update page.js

--- app/week5/page.js
+++ app/week5/page.js
@@ -4,7 +4,7 @@ import ItemList from './item-list';
 function Page() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-lime-500 to-cyan-500 py-2 ">
-      <h1 className=" text-3xl  font-bold  ml-10  text-blue-600">Shopping List</h1>
+      <h1 className=" text-3xl  font-bold  ml-10  text-black">Shopping List</h1>
       <div className="container mx-auto max-w-2xl"></div>
       <ItemList />
     </div>
