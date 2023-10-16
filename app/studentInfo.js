f3f5f562cc229d57959cdaf00ad98314ca7ffa4c
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-16 08:10:39 -0600

    Update studentInfo.js

--- app/studentInfo.js
+++ app/studentInfo.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function StudentInfo() {
     return (
-        <div className="text-center text-xl text-pink-900 font-bold" >
+        <div className="text-center font-style: italic text-xl text-pink-900 font-bold" >
             <p> Name: Mausami Desai </p>
             <p> Course Section: CPRG306 A </p>
             <Link href ="https://github.com/mosmee21/cprg306-assignments.git">https://github.com</Link>
