b87e68e18feaa85f97c131461a68392434ba42f4
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-19 23:18:10 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,14 +4,14 @@ import StudentInfo from "./studentInfo";
 export default function page() {
   return (
       <div className=" min-h-screen text-center text-violet-900 bg-gradient-to-r from-lime-500 to-cyan-500 p-10 m-10 rounded-lg font-bold ">
-        <h1 className="text-2xl mt-10 mb-10" >CPRG 306: Web Development 2 - Assignments</h1>
+        <h1 className="text-2xl xt-decoration-line: underline mt-10 mb-10" >CPRG 306: Web Development 2 - Assignments</h1>
         <StudentInfo/><br></br>
-        <Link href="./week2">week 2</Link><br></br> 
-        <Link href="./week3">week 3</Link><br></br> 
-        <Link href="./week4">week 4</Link><br></br>
-        <Link href="./week5">week 5</Link><br></br>
-        <Link href="./week6">week 6</Link>
+        <Link href="./week2" className="hover:text-white">week 2</Link><br/>
+        <Link href="./week3"className="hover:text-white">week 3</Link><br/>
+        <Link href="./week4" className="hover:text-white">week 4</Link><br/>
+        <Link href="./week5" className="hover:text-white">week 5</Link><br/>
+        <Link href="./week6"className="hover:text-white">week 6</Link>
       </div>
-  )
+  );
 }
 
