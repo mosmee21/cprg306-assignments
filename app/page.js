bfb121d2e3325de1e17ba7715aa0407db46efa23
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-20 10:24:42 -0600

    week7 added

--- app/page.js
+++ app/page.js
@@ -6,11 +6,12 @@ export default function page() {
       <div className=" min-h-screen text-center text-violet-900 bg-gradient-to-r from-lime-500 to-cyan-500 p-10 m-10 rounded-lg font-bold ">
         <h1 className="text-2xl xt-decoration-line: underline mt-10 mb-10" >CPRG 306: Web Development 2 - Assignments</h1>
         <StudentInfo/><br></br>
-        <Link href="./week2" className="hover:text-white">week 2</Link><br/>
-        <Link href="./week3"className="hover:text-white">week 3</Link><br/>
-        <Link href="./week4" className="hover:text-white">week 4</Link><br/>
-        <Link href="./week5" className="hover:text-white">week 5</Link><br/>
-        <Link href="./week6"className="hover:text-white">week 6</Link>
+        <Link href="./week2"className="hover:text-white">Week 2</Link><br/>
+        <Link href="./week3"className="hover:text-white">Week 3</Link><br/>
+        <Link href="./week4"className="hover:text-white">Week 4</Link><br/>
+        <Link href="./week5"className="hover:text-white">Week 5</Link><br/>
+        <Link href="./week6"className="hover:text-white">Week 6</Link><br/>
+        <Link href="./week7"className="hover:text-white">Week 7</Link>
       </div>
   );
 }
