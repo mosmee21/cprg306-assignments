3c6d79c2f39062747af3caf92d2000548e4c0dca
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-19 22:44:49 -0600

    Update item-list.js

--- app/week6/item-list.js
+++ app/week6/item-list.js
@@ -7,32 +7,31 @@ import Item from './item';
 function ItemList({ items }) {
   const [sortBy, setSortBy] = useState("name");
 
-
-  const sortedItems = [...items].sort((item1, item2)=> {
+  const sortedItems = [...items].sort((item1, item2) => {
     if (sortBy === "name") {
-      return  item1.name.localeCompare(item2.name);
+      return item1.name.localeCompare(item2.name);
     } else if (sortBy === "category") {
       return item1.category.localeCompare(item2.category);
     }
     return 0;
   });
-  
+
   return (
-    <div className="p-4 ">
+    <div className="p-4">
       <button
         onClick={() => setSortBy('name')}
-        className={`${sortBy === 'name' ? 'active-class' : ''} m-5 bg-orange-900 text-amber-300 px-2 py-2 mr-4`}
+        className={`${sortBy === 'name' ? 'active-class' : ''} m-5 bg-blue-900 text-white px-2 py-2 mr-4`}
       >
         Sort by Name
       </button>
       <button
         onClick={() => setSortBy('category')}
-        className={`${sortBy === 'category' ? 'active-class' : ''} bg-orange-900 text-amber-300 px-2 py-2 ml-2 `}
+        className={`${sortBy === 'category' ? 'active-class' : ''} bg-blue-900 text-white px-2 py-2 ml-2 `}
       >
         Sort by Category
       </button>
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <Item key={item.id} {...item} />
         ))}
       </ul>
@@ -48,11 +47,7 @@ ItemList.propTypes = {
       quantity: PropTypes.number.isRequired,
       category: PropTypes.string.isRequired,
     })
-  ),
-};
-
-ItemList.defaultProps = {
-  items: [],
+  ).isRequired,
 };
 
 export default ItemList;
