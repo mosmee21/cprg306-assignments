3ee8f88373d8767d14762fc271d3f18bbc706cda
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-10-24 12:37:01 -0600

    still not working

--- app/week7/meal-ideas.js
+++ app/week7/meal-ideas.js
@@ -1,38 +1,33 @@
-// meal-ideas.js
-"use client";
-
 import React, { useState, useEffect } from 'react';
 
-const fetchMealIdeas = async (ingredient) => {
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
-  const data = await response.json();
-  return data.meals;
-};
-
 function MealIdeas({ ingredient }) {
-  const [meals, setMeals] = useState([]);
+    const [meals, setMeals] = useState([]);
 
-  useEffect(() => {
-    const loadMealIdeas = async () => {
-      const fetchedMeals = await fetchMealIdeas(ingredient);
-      setMeals(fetchedMeals || []); // In case of no meals found, set to an empty array
+    const fetchMealIdeas = async (ingredient) => {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+        const data = await response.json();
+        setMeals(data.meals || []);
     };
 
-    loadMealIdeas();
-  }, [ingredient]);
+    useEffect(() => {
+        if (ingredient) {
+            fetchMealIdeas(ingredient);
+        }
+    }, [ingredient]);
 
-  return (
-    <div className="p-4 mt-5 bg-white rounded-lg shadow-md">
-      <h2 className="text-xl font-bold mb-4">Meal Ideas for {ingredient}</h2>
-      <ul>
-        {meals.map(meal => (
-          <li key={meal.idMeal} className="mb-2 text-gray-800">
-            {meal.strMeal}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Meal Ideas for {ingredient}</h2>
+            <ul>
+                {meals.map(meal => (
+                    <li key={meal.idMeal}>
+                        <img src={meal.strMealThumb} alt={meal.strMeal} width="50" height="50" />
+                        {meal.strMeal}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default MealIdeas;
--- app/week7/page.js
+++ app/week7/page.js
@@ -1,44 +1,32 @@
-// page.js
 "use client";
 
 import React, { useState } from 'react';
 import ItemList from './item-list';
 import NewItem from './new-item';
-import MealIdeas from './meal-ideas';
+import MealIdeas from './meal-idea'; // import the MealIdeas component
 import itemsData from './items.json';
 
 function Page() {
     const [items, setItems] = useState(itemsData);
-    const [selectedIngredient, setSelectedIngredient] = useState(''); // State for the chosen ingredient
+    const [selectedIngredient, setSelectedIngredient] = useState(null); // For storing selected ingredient
 
-    const handleAddItem = (newItem) => {
-      setItems([...items, newItem]);
+    const handleAddItem = (newitem) => {
+      setItems([...items, newitem]);
+    };
+
+    const handleSelectIngredient = (ingredient) => { // function to set selected ingredient
+      setSelectedIngredient(ingredient);
     };
 
     return (
-      <div className="min-h-screen bg-gradient-to-r from-lime-500 to-cyan-500 py-2 ">
-        <h1 className="text-3xl font-bold ml-10 mb-4 text-black">Shopping List</h1>
-        <NewItem onAddItem={handleAddItem} />
-        <ItemList items={items} />
-        
-        <div className="p-4 mt-5 bg-white rounded-lg shadow-md">
-          <label className="text-xl font-bold mb-4">
-            Select ingredient for meal ideas: 
-            <select 
-              className="ml-4 p-2 bg-gray-200 rounded" 
-              onChange={e => setSelectedIngredient(e.target.value)}
-            >
-              <option value="">- Select -</option>
-              {items.map(item => (
-                <option key={item.id} value={item.name.split(',')[0]}>{item.name}</option> 
-                // Taking only the primary name (before the comma if any) as ingredient
-              ))}
-            </select>
-          </label>
+        <div className="min-h-screen bg-gradient-to-r from-lime-500 to-cyan-500 py-2">
+            <h1 className="text-3xl font-bold ml-10 mb-4 text-black">Shopping List</h1>
+            <NewItem onAddItem={handleAddItem} />
+            <ItemList items={items} onSelectIngredient={handleSelectIngredient} /> {/* passing down the function */}
+            {selectedIngredient && <MealIdeas ingredient={selectedIngredient} />} {/* Conditional rendering */}
         </div>
-
-        {selectedIngredient && <MealIdeas ingredient={selectedIngredient} />}
-      </div>
     );
 }
+
 export default Page;
+
