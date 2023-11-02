f63c4bdc42793c4e91b4c86c344df9b7345d8359
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-11-02 10:46:44 -0600

    changes made

--- app/week8/page.js
+++ app/week8/page.js
@@ -1,54 +1,64 @@
-import { useUserAuth } from "../_utils/auth-context";
-import { useEffect } from "react";
-import Link from 'next/link'; // Import Link from next/link
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from 'react';
+import { useUserAuth } from './_utils/auth-context';
 
-export default function Login() {
-  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+const Page = () => {
+    const [isClient, setIsClient] = useState(false);
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
-  async function handleSignIn() {
-    try {
-      await gitHubSignIn();
-    } catch (error) {
-      console.error("Error signing in with GitHub:", error);
+    const handleLogin = async () => {
+        try {
+            await gitHubSignIn();
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+
+    const handleLogout = async () => {
+        try {
+            await firebaseSignOut();
+        } catch (error) {
+            console.error('Error during logout:', error);
+        }
+    };
+
+    if (!isClient) {
+        return <div className="flex justify-center items-center h-screen">
+                <div>Loading...</div>
+                </div>;
     }
-  }
 
-  async function handleSignOut() {
-    try {
-      await firebaseSignOut();
-    } catch (error) {
-      console.error("Error signing out:", error);
-    }
-  }
-
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      {user ? (
-        <div className="space-x-4">
-          <p>Welcome, {user.displayName} ({user.email})</p>
-          <button
-            onClick={handleSignOut}
-            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-          >
-            Sign Out
-          </button>
-          {/* Next.js Link to the shopping list page */}
-          <Link href="./shopping-list">
-            <a className="text-blue-500 hover:underline">Go to Shopping List</a>
-          </Link>
+    return (
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-900 text-white">
+           <div className="text-center">
+            {user ? (
+                <div>
+                    <h1 className="text-3xl font-semibold mb-2 hover:animate-fade-in-down">Welcome, {user.displayName}</h1>
+                    <p className="mb-4 text-gray-400 hover:animate-zoom-in">({user.email})</p>
+                    <Link href="/week8/shopping-list" className="text-blue-500 m-5 hover:text-blue-400 transition duration-500 ease-in-out hover:underline hover:scale-110 hover:shadow-lg">
+                        Go to Shopping List
+                    </Link>
+                    <button onClick={handleLogout} className="px-6 m-5 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition duration-500 ease-in-out mb-4 hover:animate-pulse">
+                        Logout
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    
+                    <button onClick={handleLogin} className="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-500 ease-in-out hover:animate-bounce">
+                        Login with GitHub
+                    </button>
+                </div>
+            )}
+          </div>
         </div>
-      ) : (
-        <button
-          onClick={handleSignIn}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Sign In with GitHub
-        </button>
-      )}
-    </div>
-  );
-}
+      );
+
+};
+
+export default Page;
