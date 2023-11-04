cfa922da4d1518606c6bb5df3e1754642b97209b
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-11-03 20:27:16 -0600

    Update page.js

--- app/week8/page.js
+++ app/week8/page.js
@@ -36,11 +36,12 @@ const Page = () => {
     return (
         <div className="flex flex-col justify-center bg-gradient-to-r from-lime-500 to-cyan-500 items-center h-screen bg-gray-900 text-white">
            <div className="text-center">
+           <h2 className="text-4xl mb-8 font-bold text-blue-900">Shopping List App</h2>
             {user ? (
                 <div>
-                    <h1 className="text-3xl text-blue-900 font-bold mb-2 ">Welcome, {user.displayName}</h1>
-                    <p className="mb-4 font-bold text-gray-900">({user.email})</p>
-                    <Link href="/week8/shopping-list" className="text-rose-900 m-5 font-bold hover:text-white hover:underline hover:scale-110 ">
+                    <h1 className="text-3xl text-blue-900 font-bold mb-8">Welcome, {user.displayName}</h1>
+                    <p className="mb-4 font-bold text-gray-900">Signed in as ({user.email})</p>
+                    <Link href="/week8/shopping-list" className="text-rose-900 m-5 mr-auto font-bold hover:text-white hover:underline hover:scale-110 ">
                         Continue to your Shopping List
                     </Link>
                     <button onClick={handleLogout} className="px-6 m-5 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition duration-500 ease-in-out mb-4 hover:animate-pulse">
