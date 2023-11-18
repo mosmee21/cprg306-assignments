62689bf15243a77c5c78057cd42181a8125de049
mosmee21 <mousamiben.desai@edu.sait.ca>
2023-11-17 17:14:22 -0700

    Week 10 added

--- app/week10/_services/shopping-list-service.js
+++ app/week10/_services/shopping-list-service.js
@@ -1,22 +1,38 @@
-import { db } from "../_utils/firebase";
-import { collection, getDocs, addDoc, query } from "firebase/firestore";
-export async function getShoppingList(userId) {
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
+import { db } from "../_utils/firebase"; // Update this path as per your project structure
+
+// Function to retrieve all items for a specific user
+async function getItems(userId) {
     const itemsArray = [];
     const itemsRef = collection(db, "users", userId, "items");
     const q = query(itemsRef);
-    const querySnapshot = await getDocs(q);
-  
-    querySnapshot.forEach((doc) => {
-      itemsArray.push({ id: doc.id, ...doc.data() });
-    });
-  
+
+    try {
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            itemsArray.push({ id: doc.id, ...doc.data() });
+        });
+    } catch (error) {
+        console.error("Error fetching items:", error);
+        // Optionally throw the error to be handled where the function is called
+        throw error;
+    }
+
     return itemsArray;
-  }
-  
-  // Function to add an item to a specific user's list in Firestore
-  export async function addItem(userId, item) {
+}
+
+// Function to add a new item to a specific user's list
+async function addItem(userId, item) {
     const itemsRef = collection(db, "users", userId, "items");
-    const docRef = await addDoc(itemsRef, item);
-  
-    return docRef.id;
-  }
+
+    try {
+        const docRef = await addDoc(itemsRef, item);
+        return docRef.id; // Returns the ID of the newly created document
+    } catch (error) {
+        console.error("Error adding new item:", error);
+        // Optionally throw the error to be handled where the function is called
+        throw error;
+    }
+}
+
+export { addItem, getItems };
